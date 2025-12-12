2db759442ff1bed48f6095a2edcde430ad0d7fab
Yike_Lin <3100281535@qq.com>
2025-12-12 16:12:46 +0800

    解决eslint名字冲突

--- eslint.config.ts
+++ eslint.config.ts
@@ -20,3 +20,9 @@ export default defineConfigWithVueTs(
   vueTsConfigs.recommended,
   skipFormatting,
 )
+
+module.exports = {
+  rules: {
+    'vue/multi-word-component-names': 'off'  // 完全关闭
+  }
+}
